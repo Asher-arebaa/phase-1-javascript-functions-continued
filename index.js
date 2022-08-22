97f1efda172bea71de0020d6867c9967a15305d9
Asher Areba <Asher.areba@student.moringaschool.com>
2022-08-22 22:53:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(defaultActivity = 'roller-skate') {
+    return `This Saturday, I want to ${defaultActivity}!`;
+}
+
+function mondayWork(defaultActivity = 'go to the office') {
+    return `This Monday, I will ${defaultActivity}.`;
+}  
+
+function wrapAdjective(emphatic){
+    return function (worker) {
+        return `You are ${emphatic}${worker}${emphatic}!`;
+    }
+}
+console.log(wrapAdjective('||'));// code your solution here
